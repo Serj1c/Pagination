dd48b1e0527cdc1594d92be9de8c48e6f54be742
Serj1c <sergej.a.isaev@gmail.com>
2020-06-03 08:06:54 +0300

    first commit

---
+++ src/Posts.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Posts = ({ posts, loading }) => {
+    if(loading) {
+        return <h2>Loading...</h2> // insert spinner here
+    }
+    return (
+        <ul className="list-group mb-4">
+            {posts.map(post => (
+                <li className="list-group-item" key={post.id}>{post.title}</li>
+            ))}
+        </ul>
+    )
+};
+
+export default Posts;
+
